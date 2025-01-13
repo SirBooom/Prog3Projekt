6b30666d90538ad58c491d3fe0795e085e0169a7
Samuel Fuchs <samuelfuchs03@gmail.com>
2025-01-13 11:21:32 +0100

    -Jooq Werbung während Build und ausführung deaktiviert -mvn package erzeugt jetzt eine Jar mit allen Dependencies

--- src/main/java/Skelleton.java
+++ src/main/java/Skelleton.java
@@ -1,5 +1,35 @@
+import org.jooq.DSLContext;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import static jooq.Tables.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static com.sun.source.doctree.DocTree.Kind.AUTHOR;
+
 public class Skelleton {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String userName = "root";
+        String password = "";
+        String url = "jdbc:SQLite:rezeptverwaltungsdb.db";
+
+        System.setProperty("org.jooq.no-logo", "true");
+        System.setProperty("org.jooq.no-tips", "true");
+
+        // Connection is the only JDBC resource that we need
+        // PreparedStatement and ResultSet are handled by jOOQ, internally
+        try {
+            Connection connection = DriverManager.getConnection(url);
+            DSLContext create = DSL.using(connection, SQLDialect.SQLITE);
+            Result<org.jooq.Record> result;
+            result = create.select().from(RECIPE).fetch();
+            System.out.println(result);
+        }
+
+        // For the sake of this tutorial, let's keep exception handling simple
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
