27f390c8ccc1eb1dffcd7ed40e114250b00cbbd6
Emir Kayaagil <emka00006@htwsaar.de>
2025-01-15 16:52:31 +0100

    Veränderung .from("RECIPE") -> .from(RECIPE)

--- src/main/java/JooqTestDrive.java
+++ src/main/java/JooqTestDrive.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import static org.jooq.impl.DSL.table;
 import static org.jooq.impl.SQLDataType.BIGINT;
 import static org.jooq.impl.SQLDataType.VARCHAR;
+import static com.example.generated.Tables.*;
 
 public class JooqTestDrive {
     public static void main(String[] args) {
@@ -16,21 +17,27 @@ public class JooqTestDrive {
         try {
             Connection connection  = DriverManager.getConnection(jdbcUrl);
             DSLContext context = DSL.using(connection);
-            Result<Record> result = context.select().from().fetch();
-            //context.insertInto(table("recipe")).values(2,"rezept2").execute();
+            Result<Record> result = context.select().from(EXAMPLE).fetch();
+            context.insertInto((RECIPE)).values(2,"rezept2").execute();
 
-            context.createTableIfNotExists("example").
+            context.createTableIfNotExists(EXAMPLE).
                     column("id", BIGINT.identity(true)).
                     column("Name", VARCHAR(25).nullable(false))
                     .constraints(DSL.constraint().primaryKey("id"))
                     .execute();
+            var selected = context.select(EXAMPLE.ID, EXAMPLE.NAME)
+                    .from(EXAMPLE)
+                    .where(EXAMPLE.ID.eq(11))
+                    .fetchOne();
 
-            context.insertInto(table("example")).
-                    values(11,"test").execute();
-            for (Record record : result) {
-                System.out.println("Record: " + record);
+            //context.insertInto(EXAMPLE)
+            //        .values(11,"test").execute();
 
-            }
+            System.out.println(selected);
+
+
+            System.out.println(result);
+
         } catch (SQLException e) {
             System.out.println("Error: Connecting to Database");
             e.printStackTrace();
