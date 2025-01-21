3f8f368e4390181baa90d6ac32b75d58f8dce815
Mohammad Moubarak <momo00013@htwsaar.de>
2025-01-21 18:42:42 +0100

    build failure -> forogt to push the skeleton file

--- src/main/java/Skeleton.java
+++ src/main/java/Skeleton.java
@@ -1,16 +1,14 @@
 import org.jooq.DSLContext;
 import org.jooq.Result;
 import org.jooq.SQLDialect;
-import org.jooq.Table;
 import org.jooq.impl.DSL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import static com.example.generated.Tables.*;
 
-import static com.sun.source.doctree.DocTree.Kind.AUTHOR;
 import static org.jooq.impl.DSL.table;
 
-public class Skelleton {
+public class Skeleton {
     public static void main(String[] args) {
         String userName = "root";
         String password = "";
@@ -24,12 +22,11 @@ public class Skelleton {
         try {
             /*
             for(int i = 1; i <= 10; i++ ) {
-                Database.insertRecipe(i, "pasta", "Italian", "dessert", "Cook then eat", 100,
+                Database.insertRecipe(i, "pasta"+ i, "Italian", "dessert", "Cook then eat", 100,
                         "15 Mins", "noodles");
             }
-
-            Database.deleteAllRecipes()
             */
+            //Database.deleteAllRecipes();
             Connection connection = DriverManager.getConnection(url);
             DSLContext create = DSL.using(connection, SQLDialect.SQLITE);
             Result<org.jooq.Record> result = create.select().from(RECIPE).fetch();
